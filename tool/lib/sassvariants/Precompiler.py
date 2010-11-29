8eec6d1ed1c43eaf3430d654f4b341309b9b1ddd
Sebastian Fastner <sf@fastnerit.de>
2010-11-29 21:10:24 +0100

    Advanced sass precompiler

---
+++ tool/lib/sassvariants/Precompiler.py
@@ -0,0 +1,113 @@
+#
+# SASS Precompiler
+#
+
+import re, os, base64
+
+class Precompiler():
+  def __init__(self, paths, variants):
+    self.__paths = paths
+    self.__variants = variants
+    self.__content = []
+
+  def run(self, filename):
+    with open(filename, encoding="utf-8") as input_file:
+      self.__input_file = input_file
+      self.__input_path = os.path.abspath(os.path.dirname(filename))
+      self.__paths = [self.__input_path] + self.__paths
+      self.__content = input_file.read().splitlines()
+      self.__token_detector("@import",self.__token_import)
+      self.__function_detector("inline_image",self.__function_inline_image)
+      self.__input_file = None
+
+  def get_content(self):
+    return "\n".join(self.__content)
+
+  def write_to_file(self, filename):
+    with open(filename, mode="w", encoding="utf-8") as output_file:
+      output_file.write(self.get_content())
+
+  def __token_detector(self, token, handler):
+    out_content = []
+    for line in self.__content:
+      matcher = re.match("^(\s*)(.*)$", line)
+      if matcher != None:
+        indents = len(matcher.group(1))
+        command = matcher.group(2)
+
+        if command[:len(token)] == token:
+          out_content.append(handler(command))
+        else:
+          out_content.append(line)
+
+      else:
+        out_content.append(line)
+    self.__content = out_content
+
+  def __function_detector(self, function, handler):
+    out_content = []
+    for line in self.__content:
+      matcher = re.match("^((\s*)(?:.*)):(?:\s*)(\S*)(?:\s*)$", line)
+      if matcher != None:
+        prefix = matcher.group(1)
+        indents = len(matcher.group(2))
+        command = matcher.group(3)
+        
+        if command[:len(function)] == function:
+          out_content.append(handler(prefix,command))
+        else:
+          out_content.append(line)
+      else:
+        out_content.append(line)
+    self.__content = out_content
+
+  def __path_finder(self, url, paths):
+    for path in paths:
+      for pathpart in ["","framework/source/resource/unify/mobile"]:
+        fname = os.path.join(path, pathpart, url)
+        if os.path.isfile(fname):
+          return path
+    return None
+
+  def __function_inline_image(self, prefix, command):
+    matcher = re.match("^inline_image\((.*)\)(\s*)$", command)
+    parameter = matcher.group(1)
+    urlmatcher = re.match(".*[\"'](.+)[\"'].*", parameter)
+    if urlmatcher != None:
+      url = urlmatcher.group(1)
+      path = self.__path_finder(url, self.__paths)
+      if path:
+        print ("Found image :", path)
+        #return prefix + ":inline_image(\"" + path + "\"," + matcher.group(1) + ")"
+        return self.__import_image(os.path.join(path, url))
+      else:
+        print ("File not found : ", url)
+    else:
+      print ("Variable detected : " , parameter)
+    return prefix + ":inline_image(\"" + self.__input_path + "\"," + matcher.group(1) + ")"
+
+  def __import_image(self, filename):
+    with open(filename, mode="rb") as image_file:
+      imgdata = image_file.read()
+    b64img = base64.encodebytes(imgdata)
+    print (b64img)
+    return str(b64img)
+
+  def __import_file(self, filename):
+    new_pre = Precompiler(self.__paths, self.__variants)
+    new_pre.run(filename)
+    return new_pre.get_content()
+
+  def __token_import(self, command):
+    command = command[8:]
+    input_file = self.__input_file
+    inputdir = os.path.dirname(input_file.name)
+    testdir = os.path.abspath(os.path.join(inputdir, command))
+    if os.path.exists(testdir):
+      return self.__import_file(testdir)
+    for path in self.__paths:
+      testdir = os.path.abspath(os.path.join(path, command))
+      if os.path.exists(testdir):
+        return self.__import_file(testdir)
+    print("File not found for @import : ", command)
+    return "// ---- NOT IMPORT: " + command
--- tool/sass.py
+++ tool/sass.py
@@ -1,87 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 
 import subprocess, os, sys, optparse, shutil
 
-def get_immediate_subdirs(path):
-    return [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
+def get_unify_path():
+    relunifypath = os.path.join(os.path.dirname(sys.argv[0]), os.pardir)
+    return os.path.abspath(relunifypath)
 
-def which_os(program):
-    import os
-    def is_exe(fpath):
-        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+sys.path.insert(0, os.path.join(get_unify_path(), "tool", "lib"))
 
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
-
-def which(program):
-    ex = which_os(program)
-    if ex != None:
-        return ex
-    else:
-        prog = program + ".exe"
-        ex = which_os(prog)
-        return ex
-
-def run_sass(osname,resourcepath,unifypath,basepath):
-    print "Processing " + osname
-    origfilename = str.replace(basepath + "/" + sys.argv[1], ".sass", "")
-    outfile = os.path.normpath(origfilename + "." + osname + ".sass")
-    genfile = os.path.normpath(origfilename + "." + osname + ".css")
-    infile = os.path.normpath(origfilename + ".sass")
-    destination = open(outfile, "w")
-    arg = ["python", unifypath + "/sass.py", outfile, genfile]
-   
-    subprocess.call(arg, cwd=unifypath)
-
-def copyover(source, osname, resource):
-    destination = open(source + "." + osname + ".sass", "w")
-    destination.write("@import " + resource + "/core.sass\n")
-    destination.write("@import " + resource + "/animation.sass\n")
-    destination.write("@import " + resource + "/" + osname + "/style.sass\n\n")
-    shutil.copyfileobj(open(source + ".sass", "r"),destination)
-    destination.close()
-
+from sassvariants.Precompiler import Precompiler
 
 def main():
-    basepath = os.getcwd() 
-    unifypath = basepath + "/" + os.path.dirname(sys.argv[0]) + "/unify-sass"
-    resourcepath = basepath + "/" + os.path.dirname(sys.argv[0]) + "/../framework/source/resource/unify/mobile"
-    resourcepath = os.path.normpath(resourcepath)
-    
-    jarpath = os.path.normpath(unifypath + "/sasscss.jar")
-    unifysasspath = os.path.normpath(unifypath + "/unify-sass.rb")
-    sourcepath = os.path.normpath(basepath + "/" + sys.argv[1])
-    
-    ruby_exec = which("ruby")
-
-    for osname in get_immediate_subdirs(resourcepath):
-        copyover(sourcepath, osname, resourcepath)
-        newsourcepath = sourcepath + "." + osname + ".sass"
-        newtargetpath = sourcepath + "." + osname + ".css"
-        if ruby_exec != None:
-            arg = [ruby_exec, unifysasspath, newsourcepath, newtargetpath]
-        else:
-            arg = ["java", "-Xms512M", "-Xmx512M", "-jar", jarpath, "-X-C", unifysasspath, newsourcepath, newtargetpath]
-  
-        print "Processing " + osname + " ..."
- 
-        subprocess.call(arg, cwd=unifypath)
+    projectpath = os.path.abspath(os.getcwd())
+    unifypath = get_unify_path()
+   
+    precompiler = Precompiler([projectpath, unifypath], ["iphoneos", "android"])
+    precompiler.run(sys.argv[1]+".sass")
+    precompiler.write_to_file(sys.argv[1]+".tmp.sass")
 
 if __name__ == '__main__':
     try:
         main()
 
     except KeyboardInterrupt:
-        print
-        print "Keyboard interrupt!"
+        print()
+        print("Keyboard interrupt!")
         sys.exit(1)
